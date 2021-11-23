a36127f407c516691e673785b39080bc514d858f
DoyenRe <doyen.re@gmail.com>
2021-11-23 14:52:28 +0100

    Implementation Victory, replay

--- tkinter_test.py
+++ tkinter_test.py
@@ -14,7 +14,7 @@ def more():
 
 root = tk.Tk()
 root.title('Juste Price - The Game')
-canvas = tk.Canvas(root, width = 1500, height=1000)
+canvas = tk.Canvas(root, width = 500, height=500)
 canvas.grid(columnspan=10, rowspan=100)
 
 
@@ -26,6 +26,7 @@ logo = ImageTk.PhotoImage(logo)
 logo_label = tk.Label(image=logo, borderwidth=0, highlightthickness=0, state='normal')
 logo_label.image = logo
 logo_label.grid(column=3, row=0)
+
 
 #Welcome Text
 welcome_text = tk.Label(root, text='Welcome in the Game Just Price! \n Hope you will get a lot of fun!', fg = 'black', font='Helvetica 25 bold')
@@ -39,25 +40,49 @@ fg = 'black', font='Raleway 23')
 instruction_text.grid(column=0, row=10)
 
 text_lbl = None
+end_game = None
+button_game_replay= None
+victory_label = None
 
 #Function Buttons
 def function_button_1():
-    global tentative, text_lbl
+    global tentative, text_lbl, end_game, button_game_replay, victory_label
     tk.Label(root, text = 'Mode Tentative', font='Raleway 23 bold').grid(column=5, row=10)
     if text_lbl is not None: 
         text_lbl.destroy()
+    if end_game is not None: 
+        end_game.destroy()
+    if button_game_replay is not None: 
+        button_game_replay.destroy()
+    if victory_label is not None: 
+        victory_label.destroy()
     entry1 = tk.Entry(root)
     canvas.create_window(200, 140, window=entry1)
     canvas.grid(column=5, row=11)
     tentative = 0
-    price = rnd.randrange(0, 100)
+   #price = rnd.randrange(0, 100)
+    price = 50
     def validate():
-        global tentative, text_lbl
+        global tentative, text_lbl, end_game, button_game_replay, victory_label
         entry_proposal = entry1.get()
         if tentative == 0:
             if int(entry_proposal) == price:
-                print('GG')
-                tentative += 1
+                end_game = tk.Label(root,text='Congratulation, you won!', fg = 'blue', font='Raleway 23')
+                end_game.grid(column=6, row=12)
+                button_game_val.destroy()
+                #Button Replay
+                button_replay_text = tk.StringVar()
+                button_game_replay = tk.Button(root, textvariable=button_replay_text, font='Times 18 bold', command=function_button_1)
+                button_replay_text.set('Replay')    
+                button_game_replay.grid(column=6, row=13)
+                #Victory
+                path_victory = '/Users/rdoyen/PythonProject/Just_Price/image/victory.png'
+                victory = Image.open(path_victory)
+                victory = victory.resize((350,350))
+                victory = ImageTk.PhotoImage(victory)
+                victory_label = tk.Label(image=victory, borderwidth=0, highlightthickness=0, state='normal')
+                victory_label.image = victory
+                victory_label.grid(column=0, row=18)
             elif int(entry_proposal) > price: 
                 text_lbl = tk.Label(root,text=less(), fg = 'red', font='Raleway 23')
                 text_lbl.grid(column=6, row=12)
@@ -68,8 +93,23 @@ def function_button_1():
                 tentative += 1
         if 0 < tentative <= 10:
             if int(entry_proposal) == price:
-                print('GG')
-                tentative += 1
+                text_lbl.destroy()
+                button_game_val.destroy()
+                end_game = tk.Label(root,text='Congratulation, you won!', fg = 'blue', font='Raleway 23')
+                end_game.grid(column=6, row=12)
+                #Button Replay
+                button_replay_text = tk.StringVar()
+                button_game_replay = tk.Button(root, textvariable=button_replay_text, font='Times 18 bold', command=function_button_1)
+                button_replay_text.set('Replay')    
+                button_game_replay.grid(column=6, row=13)
+                #Victory
+                path_victory = '/Users/rdoyen/PythonProject/Just_Price/image/victory.png'
+                victory = Image.open(path_victory)
+                victory = victory.resize((350,350))
+                victory = ImageTk.PhotoImage(victory)
+                victory_label = tk.Label(image=victory, borderwidth=0, highlightthickness=0, state='normal')
+                victory_label.image = victory
+                victory_label.grid(column=0, row=18)
             elif int(entry_proposal) > price: 
                 text_lbl.destroy()
                 text_lbl = tk.Label(root,text=less(), fg = 'red', font='Raleway 23')
@@ -83,9 +123,15 @@ def function_button_1():
         else: 
             text_lbl.destroy()
             button_game_val.destroy()
-            game_over = tk.Label(root,text='Game Over \n No more tentative', fg = 'black', font='Raleway 23')
-            game_over.grid(column=6, row=12)
-
+            end_game = tk.Label(root,text='Game Over \n No more tentative', fg = 'black', font='Raleway 23')
+            end_game.grid(column=6, row=12)
+            #Button Replay
+            button_replay_text = tk.StringVar()
+            button_game_replay = tk.Button(root, textvariable=button_replay_text, font='Times 18 bold', command=function_button_1)
+            button_replay_text.set('Replay')    
+            button_game_replay.grid(column=6, row=13)
+            print(button_game_replay)
+            print(end_game)
 
 
     #Button Validate
@@ -100,8 +146,6 @@ button_game_1_text = tk.StringVar()
 button_game_1 = tk.Button(root, textvariable=button_game_1_text, font='Times 18 bold', command=function_button_1)
 button_game_1_text.set('Mode Tentative')
 button_game_1.grid(column=0, row=15)
-
-
 
 root.mainloop()
 
